b39e384e6ed760feda1ef73caa5408c58f5bcb82
Adham-Abdelmaksoud <adham.5050@hotmail.com>
2023-01-09 22:09:11 +0200

    Added MLP max epochs hyperparameter

--- MLP.py
+++ MLP.py
@@ -13,63 +13,76 @@ DIGIT_TESTING_SIZE = 1000
 
 digits_labels_train, digits_labels_test, digits_train, digits_test = loadDigitsData(DIGIT_TRAINING_SIZE, DIGIT_TESTING_SIZE)
 
-activation_functions = ['identity', 'logistic', 'tanh', 'relu']
-learning_rates = [1, 0.1, 0.01, 0.001]
-max_accuracy = 0
-max_data = [1, 50, 'identity', 1]
-for h in range(50, 300, 50):
-    print()
-    for a in activation_functions:
-        print()
-        for l in learning_rates:
-            mlp = MLPClassifier(
-                learning_rate_init=l,
-                hidden_layer_sizes=(h,),
-                activation=a,
-                max_iter=1000
-            )
-            digits_pred = mlp.fit(digits_train, digits_labels_train).predict(digits_test)
-            acc_digit = accuracy_score(digits_labels_test, digits_pred)*100
-            print(f'Accuracy at 1 layer of {h} hidden neurons, {a} function, {l} learning rate : {round(acc_digit, 2)} %')
-            if acc_digit>max_accuracy:
-                max_accuracy = acc_digit
-                max_data = [1, h, a, l]
-    print()
-    for a in activation_functions:
-        print()
-        for l in learning_rates:
-            mlp = MLPClassifier(
-                learning_rate_init=l,
-                hidden_layer_sizes=(h,h,),
-                activation=a,
-                max_iter=1000
-            )
-            digits_pred = mlp.fit(digits_train, digits_labels_train).predict(digits_test)
-            acc_digit = accuracy_score(digits_labels_test, digits_pred)*100
-            print(f'Accuracy at 2 layers of {h} hidden neurons, {a} function, {l} learning rate : {round(acc_digit, 2)} %')
-            if acc_digit>max_accuracy:
-                max_accuracy = acc_digit
-                max_data = [2, h, a, l]
-    print()
-    for a in activation_functions:
-        print()
-        for l in learning_rates:
-            mlp = MLPClassifier(
-                learning_rate_init=l,
-                hidden_layer_sizes=(h,h,h,),
-                activation=a,
-                max_iter=1000
-            )
-            digits_pred = mlp.fit(digits_train, digits_labels_train).predict(digits_test)
-            acc_digit = accuracy_score(digits_labels_test, digits_pred)*100
-            print(f'Accuracy at 3 layers of {h} hidden neurons, {a} function, {l} learning rate : {round(acc_digit, 2)} %')
-            if acc_digit>max_accuracy:
-                max_accuracy = acc_digit
-                max_data = [3, h, a, l]
+# activation_functions = ['identity', 'logistic', 'tanh', 'relu']
+# learning_rates = [1, 0.1, 0.01, 0.001]
+# max_accuracy = 0
+# max_data = [1, 50, 'identity', 1, 200]
+# for i in [50, 200, 700]:
+#     for h in range(50, 300, 50):
+#         print()
+#         for a in activation_functions:
+#             print()
+#             for l in learning_rates:
+#                 mlp = MLPClassifier(
+#                     learning_rate_init=l,
+#                     hidden_layer_sizes=(h,),
+#                     activation=a,
+#                     max_iter=i
+#                 )
+#                 digits_pred = mlp.fit(digits_train, digits_labels_train).predict(digits_test)
+#                 acc_digit = accuracy_score(digits_labels_test, digits_pred)*100
+#                 print(f'Accuracy at 1 layer of {h} hidden neurons, {a} function, {l} learning rate, {i} max epochs : {round(acc_digit, 2)} %')
+#                 if acc_digit > max_accuracy:
+#                     max_accuracy = acc_digit
+#                     max_data = [1, h, a, l, i]
+#         print()
+#         for a in activation_functions:
+#             print()
+#             for l in learning_rates:
+#                 mlp = MLPClassifier(
+#                     learning_rate_init=l,
+#                     hidden_layer_sizes=(h,h,),
+#                     activation=a,
+#                     max_iter=i
+#                 )
+#                 digits_pred = mlp.fit(digits_train, digits_labels_train).predict(digits_test)
+#                 acc_digit = accuracy_score(digits_labels_test, digits_pred)*100
+#                 print(f'Accuracy at 2 layers of {h} hidden neurons, {a} function, {l} learning rate, {i} max epochs : {round(acc_digit, 2)} %')
+#                 if acc_digit > max_accuracy:
+#                     max_accuracy = acc_digit
+#                     max_data = [2, h, a, l, i]
+#         print()
+#         for a in activation_functions:
+#             print()
+#             for l in learning_rates:
+#                 mlp = MLPClassifier(
+#                     learning_rate_init=l,
+#                     hidden_layer_sizes=(h,h,h,),
+#                     activation=a,
+#                     max_iter=i
+#                 )
+#                 digits_pred = mlp.fit(digits_train, digits_labels_train).predict(digits_test)
+#                 acc_digit = accuracy_score(digits_labels_test, digits_pred)*100
+#                 print(f'Accuracy at 3 layers of {h} hidden neurons, {a} function, {l} learning rate, {i} max epochs : {round(acc_digit, 2)} %')
+#                 if acc_digit > max_accuracy:
+#                     max_accuracy = acc_digit
+#                     max_data = [3, h, a, l, i]
+
+# print()
+# print(f'Maximum Accuracy was {max_accuracy} % at {max_data[0]} layers of {max_data[1]} hidden neurons, {max_data[2]} function, {max_data[3]} learning rate, {max_data[4]} max epochs')
 
 print()
-print(f'Maximum Accuracy was {max_accuracy} % at {max_data[0]} layers of {max_data[1]} hidden neurons, {max_data[2]} function, {max_data[3]} learning rate')
-
+optimal = [250, 'relu', 0.01, 50]
+mlp = MLPClassifier(
+    learning_rate_init=optimal[2],
+    hidden_layer_sizes=(optimal[0]),
+    activation=optimal[1],
+    max_iter=optimal[3]
+)
+digits_pred = mlp.fit(digits_train, digits_labels_train).predict(digits_test)
+acc_digit = accuracy_score(digits_labels_test, digits_pred)*100
+print(f'Best Accuracy is {acc_digit} % at 1 layer of {optimal[0]} hidden neurons, {optimal[1]} function, {optimal[2]} learning rate, {optimal[3]} max epochs')
+print()
 
 
 print('//////////////////////////////////////////////////////////////////')
@@ -84,59 +97,74 @@ FACE_TESTING_SIZE = 150
 
 faces_labels_train, faces_labels_test, faces_train, faces_test = loadFacesData(FACE_TRAINING_SIZE, FACE_TESTING_SIZE)
 
-activation_functions = ['identity', 'logistic', 'tanh', 'relu']
-learning_rates = [1, 0.1, 0.01, 0.001]
-max_accuracy = 0
-max_data = [1, 50, 'identity', 1]
-for h in range(100, 500, 100):
-    print()
-    for a in activation_functions:
-        print()
-        for l in learning_rates:
-            mlp = MLPClassifier(
-                learning_rate_init=l,
-                hidden_layer_sizes=(h,),
-                activation=a,
-                max_iter=1000
-            )
-            faces_pred = mlp.fit(faces_train, faces_labels_train).predict(faces_test)
-            acc_faces = accuracy_score(faces_labels_test, faces_pred)*100
-            print(f'Accuracy at 1 layer of {h} hidden neurons, {a} function, {l} learning rate : {round(acc_faces, 2)} %')
-            if acc_faces>max_accuracy:
-                max_accuracy = acc_faces
-                max_data = [1, h, a, l]
-    print()
-    for a in activation_functions:
-        print()
-        for l in learning_rates:
-            mlp = MLPClassifier(
-                learning_rate_init=l,
-                hidden_layer_sizes=(h,h,),
-                activation=a,
-                max_iter=1000,
-            )
-            faces_pred = mlp.fit(faces_train, faces_labels_train).predict(faces_test)
-            acc_faces = accuracy_score(faces_labels_test, faces_pred)*100
-            print(f'Accuracy at 2 layers of {h} hidden neurons, {a} function, {l} learning rate : {round(acc_faces, 2)} %')
-            if acc_faces>max_accuracy:
-                max_accuracy = acc_faces
-                max_data = [2, h, a, l]
-    print()
-    for a in activation_functions:
-        print()
-        for l in learning_rates:
-            mlp = MLPClassifier(
-                learning_rate_init=l,
-                hidden_layer_sizes=(h,h,h,),
-                activation=a,
-                max_iter=1000
-            )
-            faces_pred = mlp.fit(faces_train, faces_labels_train).predict(faces_test)
-            acc_faces = accuracy_score(faces_labels_test, faces_pred)*100
-            print(f'Accuracy at 3 layers of {h} hidden neurons, {a} function, {l} learning rate : {round(acc_faces, 2)} %')
-            if acc_faces>max_accuracy:
-                max_accuracy = acc_faces
-                max_data = [3, h, a, l]
+# activation_functions = ['identity', 'logistic', 'tanh', 'relu']
+# learning_rates = [1, 0.1, 0.01, 0.001]
+# max_accuracy = 0
+# max_data = [1, 50, 'identity', 1]
+# for i in [50, 300, 700]:
+#     for h in range(100, 500, 100):
+#         print()
+#         for a in activation_functions:
+#             print()
+#             for l in learning_rates:
+#                 mlp = MLPClassifier(
+#                     learning_rate_init=l,
+#                     hidden_layer_sizes=(h,),
+#                     activation=a,
+#                     max_iter=i
+#                 )
+#                 faces_pred = mlp.fit(faces_train, faces_labels_train).predict(faces_test)
+#                 acc_faces = accuracy_score(faces_labels_test, faces_pred)*100
+#                 print(f'Accuracy at 1 layer of {h} hidden neurons, {a} function, {l} learning rate, {i} max epochs : {round(acc_faces, 2)} %')
+#                 if acc_faces>max_accuracy:
+#                     max_accuracy = acc_faces
+#                     max_data = [1, h, a, l, i]
+#         print()
+#         for a in activation_functions:
+#             print()
+#             for l in learning_rates:
+#                 mlp = MLPClassifier(
+#                     learning_rate_init=l,
+#                     hidden_layer_sizes=(h,h,),
+#                     activation=a,
+#                     max_iter=i,
+#                 )
+#                 faces_pred = mlp.fit(faces_train, faces_labels_train).predict(faces_test)
+#                 acc_faces = accuracy_score(faces_labels_test, faces_pred)*100
+#                 print(f'Accuracy at 2 layers of {h} hidden neurons, {a} function, {l} learning rate, {i} max epochs : {round(acc_faces, 2)} %')
+#                 if acc_faces>max_accuracy:
+#                     max_accuracy = acc_faces
+#                     max_data = [2, h, a, l, i]
+#         print()
+#         for a in activation_functions:
+#             print()
+#             for l in learning_rates:
+#                 mlp = MLPClassifier(
+#                     learning_rate_init=l,
+#                     hidden_layer_sizes=(h,h,h,),
+#                     activation=a,
+#                     max_iter=i
+#                 )
+#                 faces_pred = mlp.fit(faces_train, faces_labels_train).predict(faces_test)
+#                 acc_faces = accuracy_score(faces_labels_test, faces_pred)*100
+#                 print(f'Accuracy at 3 layers of {h} hidden neurons, {a} function, {l} learning rate, {i} max epochs : {round(acc_faces, 2)} %')
+#                 if acc_faces > max_accuracy:
+#                     max_accuracy = acc_faces
+#                     max_data = [3, h, a, l, i]
+
+# print()
+# print(f'Maximum Accuracy was {max_accuracy} % at {max_data[0]} layers of {max_data[1]} hidden neurons, {max_data[2]} function, {max_data[3]} learning rate, {max_data[3]} max epochs')
+
 
 print()
-print(f'Maximum Accuracy was {max_accuracy} % at {max_data[0]} layers of {max_data[1]} hidden neurons, {max_data[2]} function, {max_data[3]} learning rate')
+optimal = [300, 'identity', 0.1, 200]
+mlp = MLPClassifier(
+    learning_rate_init=optimal[2],
+    hidden_layer_sizes=(optimal[0], optimal[0], optimal[0]),
+    activation=optimal[1],
+    max_iter=optimal[3]
+)
+faces_pred = mlp.fit(faces_train, faces_labels_train).predict(faces_test)
+acc_digit = accuracy_score(faces_labels_test, faces_pred)*100
+print(f'Maximum Accuracy is {acc_digit} % at 3 layers of {optimal[0]} hidden neurons, {optimal[1]} function, {optimal[2]} learning rate, {optimal[3]} max epochs')
+print()
--- SVM.py
+++ SVM.py
@@ -13,92 +13,92 @@ DIGIT_TESTING_SIZE = 1000
 
 digits_labels_train, digits_labels_test, digits_train, digits_test = loadDigitsData(DIGIT_TRAINING_SIZE, DIGIT_TESTING_SIZE)
 
-POLY_DEG_N = 10
-COEFF0_N = 5
-C_list = [0.01, 10, 10000]
-gamma_list = ['scale', 'auto']
-max_accuracy = 0
-max_parameters = ['linear', 0.01, None, None, None]
+# POLY_DEG_N = 10
+# COEFF0_N = 5
+# C_list = [0.01, 10, 10000]
+# gamma_list = ['scale', 'auto']
+# max_accuracy = 0
+# max_parameters = ['linear', 0.01, None, None, None]
 
-for c in C_list:
-    print()
-    svm = SVC(kernel='linear', C=c)
+# for c in C_list:
+#     print()
+#     svm = SVC(kernel='linear', C=c)
     
-    digits_pred = svm.fit(digits_train, digits_labels_train).predict(digits_test)
+#     digits_pred = svm.fit(digits_train, digits_labels_train).predict(digits_test)
     
-    acc_digit_linear = accuracy_score(digits_labels_test, digits_pred)*100
+#     acc_digit_linear = accuracy_score(digits_labels_test, digits_pred)*100
 
-    print(f'Accuracy of Linear at regularization parameter={c} : {acc_digit_linear} %')
-    if acc_digit_linear > max_accuracy:
-        max_accuracy = acc_digit_linear
-        max_parameters = ['linear', c, None, None, None]
+#     print(f'Accuracy of Linear at regularization parameter={c} : {acc_digit_linear} %')
+#     if acc_digit_linear > max_accuracy:
+#         max_accuracy = acc_digit_linear
+#         max_parameters = ['linear', c, None, None, None]
 
-    print()
+#     print()
 
-    for c0 in range(0, COEFF0_N+1):
-        for g in gamma_list:
-            for d in range(1,POLY_DEG_N+1):
-                svm = SVC(kernel='poly', C=c, degree=d, coef0=c0, gamma=g)
+#     for c0 in range(0, COEFF0_N+1):
+#         for g in gamma_list:
+#             for d in range(1,POLY_DEG_N+1):
+#                 svm = SVC(kernel='poly', C=c, degree=d, coef0=c0, gamma=g)
 
-                digits_pred = svm.fit(digits_train, digits_labels_train).predict(digits_test)
+#                 digits_pred = svm.fit(digits_train, digits_labels_train).predict(digits_test)
 
-                acc_digit_poly = accuracy_score(digits_labels_test, digits_pred)*100
+#                 acc_digit_poly = accuracy_score(digits_labels_test, digits_pred)*100
 
-                print(f'Accuracy of Polynomial at regularization parameter={c}, degree={d}, gamma={g}, coef0={c0} : {acc_digit_poly} %')
-                if acc_digit_poly > max_accuracy:
-                    max_accuracy = acc_digit_poly
-                    max_parameters = ['poly', c, d, g, c0]
+#                 print(f'Accuracy of Polynomial at regularization parameter={c}, degree={d}, gamma={g}, coef0={c0} : {acc_digit_poly} %')
+#                 if acc_digit_poly > max_accuracy:
+#                     max_accuracy = acc_digit_poly
+#                     max_parameters = ['poly', c, d, g, c0]
 
-    print()
+#     print()
 
-    for g in gamma_list:
-        svm = SVC(kernel='rbf', C=c, gamma=g)
+#     for g in gamma_list:
+#         svm = SVC(kernel='rbf', C=c, gamma=g)
 
-        digits_pred = svm.fit(digits_train, digits_labels_train).predict(digits_test)
+#         digits_pred = svm.fit(digits_train, digits_labels_train).predict(digits_test)
 
-        acc_digit_rbf = accuracy_score(digits_labels_test, digits_pred)*100
+#         acc_digit_rbf = accuracy_score(digits_labels_test, digits_pred)*100
 
-        print(f'Accuracy of RBF at regularization parameter={c}, gamma={g} : {acc_digit_rbf} %')
-        if acc_digit_rbf > max_accuracy:
-            max_accuracy = acc_digit_rbf
-            max_parameters = ['rbf', c, None, g, None]
+#         print(f'Accuracy of RBF at regularization parameter={c}, gamma={g} : {acc_digit_rbf} %')
+#         if acc_digit_rbf > max_accuracy:
+#             max_accuracy = acc_digit_rbf
+#             max_parameters = ['rbf', c, None, g, None]
 
-    print()
+#     print()
 
-    for c0 in range(0,COEFF0_N+1):
-        for g in gamma_list:
-            svm = SVC(kernel='sigmoid', C=c, coef0=c0, gamma=g)
+#     for c0 in range(0,COEFF0_N+1):
+#         for g in gamma_list:
+#             svm = SVC(kernel='sigmoid', C=c, coef0=c0, gamma=g)
 
-            digits_pred = svm.fit(digits_train, digits_labels_train).predict(digits_test)
+#             digits_pred = svm.fit(digits_train, digits_labels_train).predict(digits_test)
 
-            acc_digit_sigmoid = accuracy_score(digits_labels_test, digits_pred)*100
+#             acc_digit_sigmoid = accuracy_score(digits_labels_test, digits_pred)*100
 
-            print(f'Accuracy of Sigmoid at regularization parameter={c}, gamma={g}, coef0={c0} : {acc_digit_sigmoid} %')
-            if acc_digit_sigmoid > max_accuracy:
-                max_accuracy = acc_digit_sigmoid
-                max_parameters = ['sigmoid', c, None, g, c0]
+#             print(f'Accuracy of Sigmoid at regularization parameter={c}, gamma={g}, coef0={c0} : {acc_digit_sigmoid} %')
+#             if acc_digit_sigmoid > max_accuracy:
+#                 max_accuracy = acc_digit_sigmoid
+#                 max_parameters = ['sigmoid', c, None, g, c0]
 
-print('\n\n')
-if max_parameters[3] == None:
-    print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}')
-elif max_parameters[4] == None:
-    print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}, gamma={max_parameters[3]}')
-elif max_parameters[2] == None:
-    print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}, gamma={max_parameters[3]}, coef0={max_parameters[4]}')
-else:
-    print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}, degree={max_parameters[2]}, gamma={max_parameters[3]}, coef0={max_parameters[4]}')
+# print('\n\n')
+# if max_parameters[3] == None:
+#     print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}')
+# elif max_parameters[4] == None:
+#     print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}, gamma={max_parameters[3]}')
+# elif max_parameters[2] == None:
+#     print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}, gamma={max_parameters[3]}, coef0={max_parameters[4]}')
+# else:
+#     print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}, degree={max_parameters[2]}, gamma={max_parameters[3]}, coef0={max_parameters[4]}')
 
 
 print()
-# optimal = ['rbf', 10, 'scale']
-# svm = SVC(kernel=optimal[0], C=optimal[1], gamma=optimal[2])
+optimal = ['rbf', 10, 'scale']
+svm = SVC(kernel=optimal[0], C=optimal[1], gamma=optimal[2])
 
-# digits_pred = svm.fit(digits_train, digits_labels_train).predict(digits_test)
+digits_pred = svm.fit(digits_train, digits_labels_train).predict(digits_test)
 
-# acc_digit = accuracy_score(digits_labels_test, digits_pred)*100
+acc_digit = accuracy_score(digits_labels_test, digits_pred)*100
 
-# print(f'Best Accuracy at kernel={optimal[0]}, regularization parameter={optimal[1]}, gamma={optimal[2]} : {acc_digit} %')
-# print()
+print(f'Best Accuracy at kernel={optimal[0]}, regularization parameter={optimal[1]}, gamma={optimal[2]} : {acc_digit} %')
+print()
 
 
 print('//////////////////////////////////////////////////////////////////')
@@ -113,89 +113,89 @@ FACE_TESTING_SIZE = 150
 
 faces_labels_train, faces_labels_test, faces_train, faces_test = loadFacesData(FACE_TRAINING_SIZE, FACE_TESTING_SIZE)
 
-POLY_DEG_N = 10
-COEFF0_N = 5
-C_list = [0.01, 10, 10000]
-gamma_list = ['scale', 'auto']
-max_accuracy = 0
-max_parameters = ['linear', 0.01, None, None, None]
+# POLY_DEG_N = 10
+# COEFF0_N = 5
+# C_list = [0.01, 10, 10000]
+# gamma_list = ['scale', 'auto']
+# max_accuracy = 0
+# max_parameters = ['linear', 0.01, None, None, None]
 
-for c in C_list:
-    print()
-    svm = SVC(kernel='linear', C=c)
+# for c in C_list:
+#     print()
+#     svm = SVC(kernel='linear', C=c)
     
-    faces_pred = svm.fit(faces_train, faces_labels_train).predict(faces_test)
+#     faces_pred = svm.fit(faces_train, faces_labels_train).predict(faces_test)
     
-    acc_face_linear = accuracy_score(faces_labels_test, faces_pred)*100
+#     acc_face_linear = accuracy_score(faces_labels_test, faces_pred)*100
 
-    print(f'Accuracy of Linear at regularization parameter={c} : {acc_face_linear} %')
-    if acc_face_linear > max_accuracy:
-        max_accuracy = acc_face_linear
-        max_parameters = ['linear', c, None, None, None]
+#     print(f'Accuracy of Linear at regularization parameter={c} : {acc_face_linear} %')
+#     if acc_face_linear > max_accuracy:
+#         max_accuracy = acc_face_linear
+#         max_parameters = ['linear', c, None, None, None]
 
-    print()
+#     print()
 
-    for c0 in range(0, COEFF0_N+1):
-        for g in gamma_list:
-            for d in range(1,POLY_DEG_N+1):
-                svm = SVC(kernel='poly', C=c, degree=d, gamma=g, coef0=c0)
+#     for c0 in range(0, COEFF0_N+1):
+#         for g in gamma_list:
+#             for d in range(1,POLY_DEG_N+1):
+#                 svm = SVC(kernel='poly', C=c, degree=d, gamma=g, coef0=c0)
 
-                faces_pred = svm.fit(faces_train, faces_labels_train).predict(faces_test)
+#                 faces_pred = svm.fit(faces_train, faces_labels_train).predict(faces_test)
 
-                acc_face_poly = accuracy_score(faces_labels_test, faces_pred)*100
+#                 acc_face_poly = accuracy_score(faces_labels_test, faces_pred)*100
 
-                print(f'Accuracy of Polynomial at regularization parameter={c}, degree={d}, gamma={g}, coef0={c0} : {acc_face_poly} %')
-                if acc_face_poly > max_accuracy:
-                    max_accuracy = acc_face_poly
-                    max_parameters = ['poly', c, d, g, c0]
+#                 print(f'Accuracy of Polynomial at regularization parameter={c}, degree={d}, gamma={g}, coef0={c0} : {acc_face_poly} %')
+#                 if acc_face_poly > max_accuracy:
+#                     max_accuracy = acc_face_poly
+#                     max_parameters = ['poly', c, d, g, c0]
 
-    print()
+#     print()
 
-    for g in gamma_list:
-        svm = SVC(kernel='rbf', C=c, gamma=g)
+#     for g in gamma_list:
+#         svm = SVC(kernel='rbf', C=c, gamma=g)
 
-        faces_pred = svm.fit(faces_train, faces_labels_train).predict(faces_test)
+#         faces_pred = svm.fit(faces_train, faces_labels_train).predict(faces_test)
 
-        acc_face_rbf = accuracy_score(faces_labels_test, faces_pred)*100
+#         acc_face_rbf = accuracy_score(faces_labels_test, faces_pred)*100
 
-        print(f'Accuracy of RBF at regularization parameter={c}, gamma={g} : {acc_face_rbf} %')
-        if acc_face_rbf > max_accuracy:
-            max_accuracy = acc_face_rbf
-            max_parameters = ['rbf', c, None, g, None]
+#         print(f'Accuracy of RBF at regularization parameter={c}, gamma={g} : {acc_face_rbf} %')
+#         if acc_face_rbf > max_accuracy:
+#             max_accuracy = acc_face_rbf
+#             max_parameters = ['rbf', c, None, g, None]
 
-    print()
+#     print()
 
-    for c0 in range(0,COEFF0_N+1):
-        for g in gamma_list:
-            svm = SVC(kernel='sigmoid', C=c, gamma=g, coef0=c0)
+#     for c0 in range(0,COEFF0_N+1):
+#         for g in gamma_list:
+#             svm = SVC(kernel='sigmoid', C=c, gamma=g, coef0=c0)
 
-            faces_pred = svm.fit(faces_train, faces_labels_train).predict(faces_test)
+#             faces_pred = svm.fit(faces_train, faces_labels_train).predict(faces_test)
 
-            acc_face_sigmoid = accuracy_score(faces_labels_test, faces_pred)*100
+#             acc_face_sigmoid = accuracy_score(faces_labels_test, faces_pred)*100
 
-            print(f'Accuracy of Sigmoid at regularization parameter={c}, gamma={g}, coef0={c0} : {acc_face_sigmoid} %')
-            if acc_face_sigmoid > max_accuracy:
-                max_accuracy = acc_face_sigmoid
-                max_parameters = ['sigmoid', c, None, g, c0]
+#             print(f'Accuracy of Sigmoid at regularization parameter={c}, gamma={g}, coef0={c0} : {acc_face_sigmoid} %')
+#             if acc_face_sigmoid > max_accuracy:
+#                 max_accuracy = acc_face_sigmoid
+#                 max_parameters = ['sigmoid', c, None, g, c0]
 
-print('\n\n')
-if max_parameters[3] == None:
-    print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}')
-elif max_parameters[4] == None:
-    print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}, gamma={max_parameters[3]}')
-elif max_parameters[2] == None:
-    print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}, gamma={max_parameters[3]}, coef0={max_parameters[4]}')
-else:
-    print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}, degree={max_parameters[2]}, gamma={max_parameters[3]}, coef0={max_parameters[4]}')
+# print('\n\n')
+# if max_parameters[3] == None:
+#     print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}')
+# elif max_parameters[4] == None:
+#     print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}, gamma={max_parameters[3]}')
+# elif max_parameters[2] == None:
+#     print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}, gamma={max_parameters[3]}, coef0={max_parameters[4]}')
+# else:
+#     print(f'Maximum Accuracy is {max_accuracy} at kernel={max_parameters[0]}, c={max_parameters[1]}, degree={max_parameters[2]}, gamma={max_parameters[3]}, coef0={max_parameters[4]}')
 
 
 print()
-# optimal = ['poly', 0.01, 8, 'scale', 1]
-# svm = SVC(kernel=optimal[0], C=optimal[1], degree=optimal[2], gamma=optimal[3], coef0=optimal[4])
+optimal = ['poly', 0.01, 8, 'scale', 1]
+svm = SVC(kernel=optimal[0], C=optimal[1], degree=optimal[2], gamma=optimal[3], coef0=optimal[4])
 
-# faces_pred = svm.fit(faces_train, faces_labels_train).predict(faces_test)
+faces_pred = svm.fit(faces_train, faces_labels_train).predict(faces_test)
 
-# acc_face = accuracy_score(faces_labels_test, faces_pred)*100
+acc_face = accuracy_score(faces_labels_test, faces_pred)*100
 
-# print(f'Best Accuracy at kernel={optimal[0]}, regularization parameter={optimal[1]}, degree={optimal[2]}, gamma={optimal[3]}, coef0={optimal[4]} : {acc_face} %')
-# print()
+print(f'Best Accuracy at kernel={optimal[0]}, regularization parameter={optimal[1]}, degree={optimal[2]}, gamma={optimal[3]}, coef0={optimal[4]} : {acc_face} %')
+print()
